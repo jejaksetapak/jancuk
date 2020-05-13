a37cd62d0ea3364d4d5e9225397516aa58b09019
Riswanto <37138545+wansr87@users.noreply.github.com>
2020-05-13 13:41:00 +0700

    Updates

---
+++ precache-manifest.b1d3ebb530882729cc3170e13e0b5697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49dc12eed85b16683e35430a2faeba1b",
+    "url": "/jejaksetapak/jancuk.git/index.html"
+  },
+  {
+    "revision": "90b8082131b4a05a58f9",
+    "url": "/jejaksetapak/jancuk.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7f8fdb2b0b360c1ce4f4",
+    "url": "/jejaksetapak/jancuk.git/static/js/2.ed4a8858.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jejaksetapak/jancuk.git/static/js/2.ed4a8858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b8082131b4a05a58f9",
+    "url": "/jejaksetapak/jancuk.git/static/js/main.392b1781.chunk.js"
+  },
+  {
+    "revision": "2f64c51a8e0cc6c94ff4",
+    "url": "/jejaksetapak/jancuk.git/static/js/runtime-main.2e481077.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jejaksetapak/jancuk.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
